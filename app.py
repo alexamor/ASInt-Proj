cb0893a7fe4bd665cd6c4f928e5f6db0b65302fa
sofiasalgueiro <sof97@live.com.pt>
2019-12-10 10:10:11 +0000

    inicio do projeto

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/canteen')
+def canteen():
+    return render_template('canteen.html')
+
+
+@app.route('/rooms')
+def rooms():
+    return render_template('rooms.html')
+
+
+if __name__ == '__main__':
+    app.run()
