11120eca3edbff2e8849f7def2bd7ab9d23af9a3
sofiasalgueiro <sof97@live.com.pt>
2019-12-14 20:25:24 +0000

    redirector

--- micro_canteen.py
+++ micro_canteen.py
@@ -4,6 +4,7 @@ import json
 
 app = Flask(__name__)
 
+
 class Meal:
     def __init__(self, type, name):
         self.type = type
@@ -12,11 +13,14 @@ class Meal:
     def __str__(self):
         return '(Type: ' + self.type + ' name: ' + self.name + ')'
 
+
 @app.route('/api/canteen', methods=['GET'])
 def get_fenix():
     # retirar o dia do pedido get
-    day = request.args.get('day', '')
+    auxday = request.args.get('day', '')
 
+    day = auxday.replace('-','/')
+    print(day)
     # enviar pedido get à api do tecnico
     url = "https://fenix.tecnico.ulisboa.pt/api/fenix/v1/canteen/"
     r = requests.get(url)
@@ -31,7 +35,7 @@ def get_fenix():
 
     for m in data:
         if m['day'] == day:
-            #print(m)
+            # print(m)
             for me in m['meal']:
                 if me['type'] == 'Almoço':
                     auxMeal = lunch
@@ -41,9 +45,12 @@ def get_fenix():
                 for pl in me['info']:
                     auxMeal.append(Meal(pl['type'], pl['name']))
 
+    retStr = ''
+    for m in lunch:
+        retStr = retStr + str(m) + '\n'
 
-    return 'OK'
-
+    #return {'res':retStr}
+    return json.dumps(r.json())
 
 if __name__ == '__main__':
     app.debug = True
---
+++ redirector.py
@@ -0,0 +1,66 @@
+import json
+
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/redirect', methods=['POST', 'GET'])
+def redirect():
+    # processa o tipo de pedido
+    if request.method == 'POST':
+
+        service_id = request.form['service_id']
+
+        # abre os serviços possíveis
+        fh = open("microservices.txt", 'r')
+
+        # found microservice
+        found = False
+
+        # le todas as linhas
+        lines = fh.readlines()
+        for line in lines:
+            aux = line.split()
+            if aux[0] == service_id:
+                url = "http://127.0.0.1:5000/api/" + aux[1]
+                found = True
+                break
+
+        if found:
+            data = request.form
+            requests.post(url, data=data)
+            return 'OK'
+        else:
+            return 'Your request cannot be processed.'
+    elif request.method == 'GET':
+
+        service_id = request.args.get('serviceId', 'id')
+        # abre os serviços possíveis
+        fh = open("microservices.txt", 'r')
+
+        # found microservice
+        found = False
+
+        # le todas as linhas
+        lines = fh.readlines()
+        for line in lines:
+            aux = line.split()
+            if aux[0] == service_id:
+                print(aux[1])
+                port = str(5000 + int(service_id))
+                url = "http://127.0.0.1:" + port + "/api/" + aux[1].strip('\n')
+                found = True
+                break
+
+        if found:
+            data = request.args.to_dict()
+            print(data)
+            r = requests.get(url, params=data)
+            print(r.status_code)
+            response = r.json()
+            print(response)
+            return json.dumps(r.json())
+        else:
+            return 'Your request cannot be processed.'
