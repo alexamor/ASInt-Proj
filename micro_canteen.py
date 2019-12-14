11699e484a256772f0ddda70e7b9062c3e2c0a14
sofiasalgueiro <sof97@live.com.pt>
2019-12-14 14:11:04 +0000

    update da canteen

--- micro_canteen.py
+++ micro_canteen.py
@@ -3,23 +3,26 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/api/canteen', methods = ['GET'])
+
+@app.route('/api/canteen', methods=['GET'])
 def get_fenix():
     # retirar o dia do pedido get
-    day = request.form['day']
+    day = request.args.get('day', '')
 
     # enviar pedido get à api do tecnico
     url = "https://fenix.tecnico.ulisboa.pt/api/fenix/v1/canteen/"
     r = requests.get(url)
-
 
     print(r.status_code)
 
     # recebe os dados do fenix
     data = r.json()
 
-
     print(data)
 
+    return 'OK'
+
+
 if __name__ == '__main__':
+    app.debug = False
     app.run()
