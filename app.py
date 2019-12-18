755ee950e0bcdb883958776676c9594e567103a0
alexamor <alexvfil@gmail.com>
2019-12-18 19:34:38 +0000

    secrets

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import random
+
 from flask import Flask, render_template, request, jsonify, redirect, session
 import requests
+
+class User:
+    def __init__(self, name, photo, token):
+        self.name = name
+        self.token = token
+        self.photo = photo
+        self.secret = None
 
 redirect_uri = "http://127.0.0.1:8000/userAuth" # this is the address of the page on this app
 
@@ -9,8 +18,9 @@ clientSecret = "rcub4Vdgb20G+14SbCKdktzN8pmP8xW/40OxvQSg0Mxo8lcsGQl1sOfflV0Vuhgj
 fenixLoginpage= "https://fenix.tecnico.ulisboa.pt/oauth/userdialog?client_id=%s&redirect_uri=%s"
 fenixacesstokenpage = 'https://fenix.tecnico.ulisboa.pt/oauth/access_token'
 
-loginName = []
-userToken = []
+#loginName = []
+#userToken = []
+userList = []
 code = False
 app = Flask(__name__)
 app.secret_key = "ASINT"
@@ -38,6 +48,39 @@ def qr():
     return render_template('qr.html')
 
 ######### FENIX LOGIN #################
+@app.route('/readsecret', methods=['POST', 'GET'])
+def readsecret():
+    if request.method == 'POST':
+        selSecret = request.form['secret']
+        print("sel: " + selSecret + "  type:  " + type(selSecret))
+        friend = None
+
+        for x in userList:
+            if x.secret == int(selSecret):
+                print("x.secret: " + x.secret + "  type:  " + type(x.secret))
+                friend = x
+                return render_template('showUser.html', name=friend.name, photo=friend.photo)
+
+        return "Secret not found"
+
+    else:
+        return render_template('readsecret.html')
+
+@app.route('/getsecret')
+def getsecret():
+    if session.get('token') is None:
+        redPage = fenixLoginpage % (client_id, redirect_uri)
+        # the app redirects the user to the FENIX login page
+        return redirect(redPage)
+    else:
+        secret = random.randint(1, 10000000)
+
+        for x in userList:
+            if x.token == session.get('token'):
+                x.secret = secret
+                break
+
+        return render_template('getsecret.html', secret=secret)
 
 @app.route('/private')
 def private_page():
@@ -62,13 +105,14 @@ def private_page():
             r_info = resp.json()
             print( r_info)
 
-            i= 0
-            for x in userToken:
-                if x == session.get('token'):
+            #i= 0
+            for x in userList:
+                if x.token == session.get('token'):
+                    istid = x.name
                     break
-                i += 1
+                #i += 1
 
-            istid = loginName[i]
+            #istid = loginName[i]
 
             return render_template("privPage.html", username=istid, name=r_info['name'])
         else:
@@ -100,10 +144,12 @@ def userAuthenticated():
         print( r_info)
 
         # we store it
-        global loginName
-        loginName.append(r_info['username'])
-        global userToken
-        userToken.append(r_token['access_token'])
+        global userList
+        userList.append(User(r_info['displayName'], r_info['photo']['data'], r_token['access_token']))
+        #global loginName
+        #loginName.append(r_info['username'])
+        #global userToken
+        #userToken.append(r_token['access_token'])
 
         session['key'] = r_token['access_token']
         session['token'] = r_token['access_token']
@@ -118,13 +164,14 @@ def userAuthenticated():
 
 @app.route('/admin')
 def admin():
-    i = 0
-    for x in userToken:
-        if x == session.get('token'):
+    #i = 0
+    for x in userList:
+        if x.token == session.get('token'):
+            istid = x.name
             break
-        i += 1
+        #i += 1
 
-    istid = loginName[i]
+    #istid = loginName[i]
     return render_template("appPage.html", username=istid)
 
 if __name__ == '__main__':
